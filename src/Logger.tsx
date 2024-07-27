3d42fde07d3acc783aa90ee03c477b1f3df6e3d7
Tgn <114723993+tgnlex@users.noreply.github.com>
2024-07-27 19:10:21 -0400

    Update Logger.tsx

--- src/Logger.tsx
+++ src/Logger.tsx
@@ -1,3 +1,28 @@
 interface LogFn {
+  (message?: any, ...opts: any[])
 }
-class Logger
+class Logger {
+  log: LogFn
+  debug: LogFn
+  info: LogFn
+  warn: LogFn
+  error: LogFn
+}
+class ConsoleLogger implements Logger {
+  log(message?: any, opts: any[]): void{
+    console.log(message, ...opts)
+  }
+  debug(message?: any, opts: any[]): void {
+    console.debug(message, ...opts)
+  }
+  info(message?: any, opts: any[]): void {
+    console.info(meessage, ...opts)
+  }
+  warn(message?: any, opts: any[]): void {
+    console.warn(message, ...opts)
+  }
+  error(message?: any, opts: any[]): void {
+    console.error(message, ...opts)
+  }
+
+}
